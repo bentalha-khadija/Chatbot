2b61c896760be9feae2ce8f7af091095ac36ac64
hamid abdellaoui <h.abdellaoui@insea.ac.ma>
2022-09-09 00:08:26 +0100

    updates

--- actions/actions.py
+++ actions/actions.py
@@ -1,5 +1,5 @@
 from typing import Text, List, Any, Dict
-import json, re
+import json, re, jsonlines
 from rasa_sdk import Tracker, FormValidationAction, Action
 from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
@@ -25,7 +25,7 @@ class ValidateClientForm(FormValidationAction):
         if re.match(r"[^@]+@[^@]+\.[^@]+", text):
             return {"email": slot_value}
         else:
-            dispatcher.utter_message(text="Veuilez entrer une adresse email valide")
+            dispatcher.utter_message(text=f"{slot_value} n'est pas valide")
             return {"email": None}
 
     def submit(
@@ -38,9 +38,8 @@ class ValidateClientForm(FormValidationAction):
             after all required slots are filled"""
         # export the form reponses into a json file 
         # and save it in the current directory 
-        # with the name `responses.json`
-        with open("responses.json", "w") as outfile:
-            json.dump(tracker.form_slots_to_validate(), outfile)
-        dispatcher.utter_message(text=f"Thanks for your order!")
+        # with the name `responses.json` using jsonlines
+        with jsonlines.open('responses.json', mode='w') as writer:
+            writer.write(tracker.current_slot_values())
         return []
     
